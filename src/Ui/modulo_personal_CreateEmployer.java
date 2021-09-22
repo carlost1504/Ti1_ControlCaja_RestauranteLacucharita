38426de1da3ff758053002bdd1c31558c71baa1a
carlost1504 <61698120+carlost1504@users.noreply.github.com>
2021-09-22 09:55:30 -0500

    add the format save in byte

--- src/Ui/modulo_personal_CreateEmployer.java
+++ src/Ui/modulo_personal_CreateEmployer.java
@@ -1,44 +1,65 @@
 package Ui;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+import javafx.collections.FXCollections;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.SplitMenuButton;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import model.Employer;
 
 public class modulo_personal_CreateEmployer extends Stage {
 	
 	
 	 
-	    private SplitMenuButton Split_typeEmployer;
+    private TextField tx_nameEmployer;
 
-	    private TextField tx_nameEmployer;
+    private TextField tx_IdEmployer;
 
-	    private TextField tx_IdEmployer;
+    private PasswordField tx_passwordEmployer;
 
-	    private PasswordField tx_passwordEmployer;
+    private Button Bt_return;
 
-	    private Button Bt_return;
+    private Button Bt_home;
 
-	    private Button Bt_home;
+    private Button bt_addEmployer;
 
-	    private Button bt_addEmployer;
+    private ChoiceBox<?> List_typeEmployer;
+    
+    private DatePicker DateBox;
 	    
 	    public modulo_personal_CreateEmployer() {
 	    	try {
 				FXMLLoader loader = new FXMLLoader(getClass().getResource("modulo_personal_CreateEmployer.fxml"));
 		        Parent parent = loader.load();
 				
-		        Split_typeEmployer=(SplitMenuButton)loader.getNamespace().get("Split_typeEmployer");
+		        
+		        
 		        tx_nameEmployer=(TextField)loader.getNamespace().get("tx_nameEmployer");
+		        tx_IdEmployer=(TextField)loader.getNamespace().get("tx_IdEmployer");
 		        tx_passwordEmployer=(PasswordField)loader.getNamespace().get("tx_passwordEmployer");
 		        Bt_return=(Button)loader.getNamespace().get("Bt_return");
 		        Bt_home=(Button)loader.getNamespace().get("Bt_home");
 		        bt_addEmployer=(Button)loader.getNamespace().get("bt_addEmployer");
+		        bt_addEmployer=(Button)loader.getNamespace().get("bt_addEmployer");
+		        DateBox=(DatePicker)loader.getNamespace().get("DateBox");
+		        
+		        
+		        
+		        List_typeEmployer = new ChoiceBox (FXCollections.observableArrayList ("", "Lb", "units", "L", "lm"));
+		        
+		        Employer=new Employer(getFullScreenExitHint(), getFullScreenExitHint(), getTitle(), getFullScreenExitHint());
 
 				Scene scene=new Scene(parent,679,600);
 				setScene(scene);
@@ -67,11 +88,22 @@ public class modulo_personal_CreateEmployer extends Stage {
 	    }
 	    public void addEmployerr() {
 	    	bt_addEmployer.setOnAction(Event->{
-	    		modulo_personal_CreateEmployer p=new modulo_personal_CreateEmployer();
-				Modulos m=new Modulos();
-				m.show();
-				p.close();
+	    		
 			});
 	    }
+	    
+	    private void SaveJavaByteCode() {
+	    	try {
+	        	File ref = new File("Inventary.temp");
+				FileOutputStream fos = new FileOutputStream(ref);
+				ObjectOutputStream oos = new ObjectOutputStream(fos);
+				oos.writeObject(inventary);
+				oos.close();	
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+	    	
+		}
     
 }
